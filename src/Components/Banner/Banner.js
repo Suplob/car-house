8f0135379d6a1f4d7947cc48c4a2f7468920abae
Suplob <parbaroy30@gmail.com>
2022-01-06 21:09:00 +0600

    banner improved

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,30 +1,39 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, Box } from "@mui/material";
 import React from "react";
 
 const Banner = () => {
+  const backgroundStyle = {
+    background:
+      "url(https://image.freepik.com/free-vector/blue-curve-background_53876-113112.jpg)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    marginTop: "-80px",
+  };
   return (
-    <Container sx={{ mt: 10 }}>
-      <Grid
-        container
-        spacing={2}
-        sx={{ display: "flex", alignItems: "center" }}
-      >
-        <Grid item xs={12} md={6}>
-          <Typography variant="h2">Want to buy car?</Typography>
-          <Typography variant="h5">
-            You are in the right place to buy car
-          </Typography>
-        </Grid>
+    <Box style={backgroundStyle}>
+      <Container sx={{ mt: 10 }}>
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: "flex", alignItems: "center" }}
+        >
+          <Grid item xs={12} md={6} sx={{ textAlign: "start" }}>
+            <Typography variant="h2">Want to buy car?</Typography>
+            <Typography variant="h5">
+              You are in the right place to buy car
+            </Typography>
+          </Grid>
 
-        <Grid item xs={12} md={6}>
-          <img
-            src="https://i.pinimg.com/originals/91/06/02/910602979bda92b9f88144d313f52725.png"
-            alt="car"
-            style={{ width: "100%" }}
-          />
+          <Grid item xs={12} md={6}>
+            <img
+              src="https://i.pinimg.com/originals/91/06/02/910602979bda92b9f88144d313f52725.png"
+              alt="car"
+              style={{ width: "100%" }}
+            />
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Box>
   );
 };
 
